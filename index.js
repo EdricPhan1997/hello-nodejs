34e51f2960cc9561487738faf707793fb86a6e24
Hieu-Noir <hieukebab2020@gmail.com>
2023-03-30 10:13:57 +0700

    update code 29/3

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Hello World
+ */
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(3000, function () {
+  console.log("App listening on port 3000!");
+});
